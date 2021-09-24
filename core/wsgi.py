6d97e6834651aca0a4b6bda796761bc17bd47b38
Aleksey Korshuk <ale-kor02@mail.ru>
2021-09-24 14:17:03 +0300

    feat: analysis

--- core/wsgi.py
+++ core/wsgi.py
@@ -4,9 +4,12 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 import os
-
+from apps.app.controller import start
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+
+start()
